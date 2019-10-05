04ce29f71e0a7643572248692d8053454fe9b8ff
ahtee <otteb1@nku.edu>
2019-10-05 15:13:01 -0400

    removing notfound route

--- src/App.js
+++ src/App.js
@@ -53,7 +53,6 @@ function App() {
             <Route path="/projects" component={Projects} />
             <Route path="/blog" component={Blog} />
             <Route path="/blog/:id" component={Blog} />
-            <Route component={NotFound} />
           </Switch>
           <Footer />
         </Router>
