204ca6636281dbe96b35aeb20d43f8d67aa9a2c7
ahtee <otteb1@nku.edu>
2019-10-27 11:40:51 -0400

    Adding helmets to each page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
 function Portfolio() {
-  return <div>Portfolio</div>
+  return (
+    <div>
+      <Helmet>
+        <title>otte.io | Portfolio</title>
+        <meta name="description" content="otte.io work portfolio" />
+      </Helmet>
+      Portfolio
+    </div>
+  )
 }
 export default Portfolio
---
+++ src/pages/Resume.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+function Resume() {
+  return (
+    <div>
+      <Helmet>
+        <title>otte.io | Resume</title>
+        <meta name="description" content="otte.io view or download resume" />
+      </Helmet>
+      Resume
+    </div>
+  )
+}
+export default Resume
