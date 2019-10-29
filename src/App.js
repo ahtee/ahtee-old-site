1749d51dc4f8df8add6e39a4cf68cc5d780830dd
212615385 <Benjamin.Otte@ge.com>
2019-10-29 10:55:14 -0400

    Adds lazy load to Page component, which sets the minHeight

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, { useState } from 'react'
 import { Router } from '@reach/router'
 import './App.css'
 import Loading from './components/Loading'
-import Page from './components/Page'
-import Login from './pages/Login'
 const Blog = React.lazy(() => import('./pages/Blog'))
 const Contact = React.lazy(() => import('./pages/Contact'))
 const Footer = React.lazy(() => import('./components/Footer'))
 const Home = React.lazy(() => import('./pages/Home'))
 const History = React.lazy(() => import('./pages/History'))
+const Login = React.lazy(() => import('./pages/Login'))
 const Navigation = React.lazy(() => import('./components/Navigation'))
 const NotFound = React.lazy(() => import('./pages/NotFound'))
+const Page = React.lazy(() => import('./components/Page'))
 const Portfolio = React.lazy(() => import('./pages/Portfolio'))
 const Resume = React.lazy(() => import('./pages/Resume'))
 
@@ -39,16 +39,18 @@ function App() {
         />
         <React.Suspense fallback={<Loading />}>
           <Page>
-            <Router primary={false}>
-              <Blog path="/blog" />
-              <Contact path="/contact" />
-              <History path="/history" />
-              <Home path="/" />
-              <Login path="/login" />
-              <Portfolio path="/portfolio" />
-              <Resume path="/resume" />
-              <NotFound default />
-            </Router>
+            <React.Suspense fallback={<Loading />}>
+              <Router primary={false}>
+                <Blog path="/blog" />
+                <Contact path="/contact" />
+                <History path="/history" />
+                <Home path="/" />
+                <Login path="/login" />
+                <Portfolio path="/portfolio" />
+                <Resume path="/resume" />
+                <NotFound default />
+              </Router>
+            </React.Suspense>
           </Page>
           <React.Suspense fallback={<Loading />}>
             <Footer />
